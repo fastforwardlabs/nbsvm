f749a9ffbfba3f7f0db26ed680e1c7cee5130b4b
Chris Wallace <chris@bitcollider.io>
2019-08-12 19:51:43 +0100

    Reformat to mostly PEP8
    A foolish consistency is the hobgoblin of little minds

--- nbsvm/_nbsvm.py
+++ nbsvm/_nbsvm.py
@@ -14,10 +14,10 @@ import scipy
 
 
 class NBSVM(BaseEstimator, LinearClassifierMixin):
-	"""
-	A NBSVM classifier following the sklearn API, as described in Section 2.3 of
-	Baselines and bigrams: simple, good sentiment and topic classification.
-	https://nlp.stanford.edu/pubs/sidaw12_simple_sentiment.pdf
+    """
+    A NBSVM classifier following the sklearn API, as described in Section 2.3
+    of Baselines and bigrams: simple, good sentiment and topic classification.
+    https://nlp.stanford.edu/pubs/sidaw12_simple_sentiment.pdf
 
     Parameters
     ----------
@@ -35,29 +35,27 @@ class NBSVM(BaseEstimator, LinearClassifierMixin):
     y_ : ndarray, shape (n_samples,)
         The labels passed during :meth:`fit`.
     classes_ : ndarray, shape (n_classes,)
-		The classes seen at :meth:`fit`.
-	coef_ : array, shape = [1, n_features] if n_classes == 2
-			else [n_classes, n_features]
-		Weights assigned to the features, per sklearn.svm.LinearSVC.
-	intercept_ : array, shape = [1] if n_classes == 2 else [n_classes]
-		Constants in decision function, per sklearn.svm.LinearSVC.
-	"""
+        The classes seen at :meth:`fit`.
+    coef_ : array, shape = [1, n_features] if n_classes == 2
+            else [n_classes, n_features]
+        Weights assigned to the features, per sklearn.svm.LinearSVC.
+    intercept_ : array, shape = [1] if n_classes == 2 else [n_classes]
+        Constants in decision function, per sklearn.svm.LinearSVC.
+    """
 
+    def __init__(self, alpha=1.0, beta=0.25, C=1.0):
+        self.alpha = alpha
+        self.beta = beta
+        self.C = C
 
-	def __init__(self, alpha=1.0, beta=0.25, C=1.0):
-		self.alpha = alpha
-		self.beta = beta
-		self.C = C
-
-
-	def fit(self, X, y):
-		"""
-		Fit the NBSVM to a dataset.
+    def fit(self, X, y):
+        """
+        Fit the NBSVM to a dataset.
 
         Parameters
         ----------
-		X : scipy.sparse.csr_matrix or numpy.ndarray,
-			shape (n_samples, n_features)
+        X : scipy.sparse.csr_matrix or numpy.ndarray,
+            shape (n_samples, n_features)
             The training input samples.
         y : array-like, shape (n_samples,)
             The target values. An array of int.
@@ -66,98 +64,91 @@ class NBSVM(BaseEstimator, LinearClassifierMixin):
         -------
         self : object
             Returns self.
-    	"""
-		X, y = self._validate(X, y)
+        """
+        X, y = self._validate(X, y)
 
-		self.classes_ = unique_labels(y)
+        self.classes_ = unique_labels(y)
 
-		coefficients, intercepts = self._fit_one_model_per_class(X, y)
+        coefficients, intercepts = self._fit_one_model_per_class(X, y)
 
-		self.coef_ = np.concatenate(coefficients)
-		self.intercept_ = np.concatenate(intercepts)
+        self.coef_ = np.concatenate(coefficients)
+        self.intercept_ = np.concatenate(intercepts)
 
-		return self
+        return self
 
+    def _validate(self, X, y):
+        """
+        Validate that X and y are the correct shape, and that X contains no
+        negative entries.
+        """
+        X, y = check_X_y(X, y, accept_sparse="csr")
 
-	def _validate(self, X, y):
-		"""
-		Validate that X and y are the correct shape, and that X contains no
-		negative entries.
-		"""
-		X, y = check_X_y(X, y, accept_sparse="csr")
+        if scipy.sparse.issparse(X):
+            self._validate_sparse(X)
+        elif isinstance(X, np.ndarray):
+            self._validate_dense(X)
+        else:
+            raise ValueError("""
+                Not a scipy.sparse.csr.csr_matrix or numpy ndarray
+            """)
 
-		if scipy.sparse.issparse(X):
-			self._validate_sparse(X)
-		elif isinstance(X, np.ndarray):
-			self._validate_dense(X)
-		else:
-			raise ValueError("""
-				Not a scipy.sparse.csr.csr_matrix or numpy ndarray
-			""")
+        return X, y
 
-		return X, y
+    def _validate_sparse(self, X):
+        if (X.data < 0.0).any():
+            raise ValueError("All X entries should be non-negative")
 
+    def _validate_dense(self, X):
+        if (X < 0.0).any():
+            raise ValueError("All X entries should be non-negative")
 
-	def _validate_sparse(self, X):
-		if (X.data < 0.0).any():
-			raise ValueError("All X entries should be non-negative")
+    def _fit_one_model_per_class(self, X, y):
+        """
+        Treat an n-class classification problem as n binary classification
+        problems.
+        """
+        binary_models = [
+            self._fit_binary_nbsvm(X, y == class_) for class_ in self.classes_
+        ]
+        coefficients, intercepts = zip(*binary_models)
+        return coefficients, intercepts
 
+    def _fit_binary_nbsvm(self, X, y):
+        """
+        Fit a NBSVM classifier to a binary classification problem.
+        """
+        r = self._log_count_ratio(X, y)
 
-	def _validate_dense(self, X):
-		if (X < 0.0).any():
-			raise ValueError("All X entries should be non-negative")
+        X = X.multiply(r)
 
+        svm = LinearSVC(C=self.C).fit(X, y)
 
-	def _fit_one_model_per_class(self, X, y):
-		"""
-		Treat an n-class classification problem as n binary classification
-		problems.
-		"""
-		binary_models = [
-			self._fit_binary_nbsvm(X, y == class_) for class_ in self.classes_
-		]
-		coefficients, intercepts = zip(*binary_models)
-		return coefficients, intercepts
+        coef = self._interpolate(svm.coef_)
+        coef *= r
 
+        return coef, svm.intercept_
 
-	def _fit_binary_nbsvm(self, X, y):
-		"""
-		Fit a NBSVM classifier to a binary classification problem.
-		"""
-		r = self._log_count_ratio(X, y)
+    def _log_count_ratio(self, X, y):
+        """
+        Log-count ratio computed from smoothed (by alpha) count vectors for
+        each class. These are the coefficients in pure Multinomial Naive Bayes.
+        """
+        p = self.alpha + X[y == 1].sum(axis=0)
+        q = self.alpha + X[y == 0].sum(axis=0)
+        r = (self._log_normalize_count_vector(p) -
+             self._log_normalize_count_vector(q))
+        return r
 
-		X = X.multiply(r)
+    def _log_normalize_count_vector(self, arr):
+        """
+        Takes count vector and normalizes by L1 norm, then takes log.
+        """
+        return np.log(arr / np.linalg.norm(arr, 1))
 
-		svm = LinearSVC(C=self.C).fit(X, y)
-
-		coef = self._interpolate(svm.coef_)
-		coef *= r
-
-		return coef, svm.intercept_
-
-
-	def _log_count_ratio(self, X, y):
-		"""
-		Log-count ratio computed from smoothed (by alpha) count vectors for each
-		class. These are the coefficients in pure Multinomial Naive Bayes.
-		"""
-		p = self.alpha + X[y == 1].sum(axis=0)
-		q = self.alpha + X[y == 0].sum(axis=0)
-		r = (self._log_normalize_count_vector(p)
-			 - self._log_normalize_count_vector(q))
-		return r
-
-
-	def _log_normalize_count_vector(self, arr):
-		"""
-		Takes count vector and normalizes by L1 norm, then takes log.
-		"""
-		return np.log(arr / np.linalg.norm(arr, 1))
-
-
-	def _interpolate(self, coef):
-		"""
-		Interpolate with parameter beta between Multinomial Naive Bayes		(mean_weight) and SVM.
-		"""
-		mean_weight = np.abs(coef).mean()
-		return self.beta * coef + (1 - self.beta) * mean_weight
+    def _interpolate(self, coef):
+        """
+        Interpolate with parameter beta between Multinomial Naive Bayes
+        (mean_weight) and SVM.
+        """
+        mean_weight = np.abs(coef).mean()
+        return self.beta * coef + (1 - self.beta) * mean_weight
--- nbsvm/tests/test_nbsvm.py
+++ nbsvm/tests/test_nbsvm.py
@@ -9,73 +9,73 @@ from nbsvm import NBSVM
 
 
 def newsgroups(vectorizer):
-	train = fetch_20newsgroups(
-		subset="train",
-		categories=["alt.atheism", "sci.space", "talk.politics.misc"]
-	)
-	test = fetch_20newsgroups(
-		subset="test",
-		categories=["alt.atheism", "sci.space", "talk.politics.misc"]
-	)
-	train_X = vectorizer.fit_transform(train.data)
-	train_y = train.target
-	test_X = vectorizer.transform(test.data)
-	test_y = test.target
-	return train_X, train_y, test_X, test_y
+    train = fetch_20newsgroups(
+        subset="train",
+        categories=["alt.atheism", "sci.space", "talk.politics.misc"]
+    )
+    test = fetch_20newsgroups(
+        subset="test",
+        categories=["alt.atheism", "sci.space", "talk.politics.misc"]
+    )
+    train_X = vectorizer.fit_transform(train.data)
+    train_y = train.target
+    test_X = vectorizer.transform(test.data)
+    test_y = test.target
+    return train_X, train_y, test_X, test_y
 
 
 @pytest.fixture
 def count_newsgroups():
-	return newsgroups(CountVectorizer(binary=True))
+    return newsgroups(CountVectorizer(binary=True))
 
 
 @pytest.fixture
 def tfidf_newsgroups():
-	return newsgroups(TfidfVectorizer())
+    return newsgroups(TfidfVectorizer())
 
 
 def test_NBSVM_initializes_with_params():
-	clf = NBSVM(alpha=0.1, beta=0.2, C=0.3)
-	assert clf.alpha == 0.1
-	assert clf.beta == 0.2
-	assert clf.C == 0.3
+    clf = NBSVM(alpha=0.1, beta=0.2, C=0.3)
+    assert clf.alpha == 0.1
+    assert clf.beta == 0.2
+    assert clf.C == 0.3
 
 
 def test_NBSVM_raises_on_dense_negative_X():
-	X = [[1., -1.]]
-	y = [1]
-	clf = NBSVM()
-	with pytest.raises(ValueError):
-		clf.fit(X,y)
+    X = [[1., -1.]]
+    y = [1]
+    clf = NBSVM()
+    with pytest.raises(ValueError):
+        clf.fit(X, y)
 
 
 def test_NBSVM_raises_on_sparse_negative():
-	X = scipy.sparse.csr_matrix([[1., -1.]])
-	y = scipy.sparse.csr_matrix([1])
-	clf = NBSVM()
-	with pytest.raises(ValueError):
-		clf.fit(X,y)
+    X = scipy.sparse.csr_matrix([[1., -1.]])
+    y = scipy.sparse.csr_matrix([1])
+    clf = NBSVM()
+    with pytest.raises(ValueError):
+        clf.fit(X, y)
 
 
 def test_NBSVM_extracts_classes(count_newsgroups):
-	X, y, _, _ = count_newsgroups
-	clf = NBSVM()
-	clf.fit(X, y)
-	assert hasattr(clf, 'classes_')
-	assert len(clf.classes_) == 3
+    X, y, _, _ = count_newsgroups
+    clf = NBSVM()
+    clf.fit(X, y)
+    assert hasattr(clf, 'classes_')
+    assert len(clf.classes_) == 3
 
 
-def test_NBSVM_scores_well_on_test(count_newsgroups):
-	train_X, train_y, test_X, test_y = count_newsgroups
-	clf = NBSVM()
-	clf.fit(train_X, train_y)
-	p = clf.predict(test_X)
-	assert accuracy_score(p, test_y) > 0.9
+def test_NBSVM_scores_well_with_count_features(count_newsgroups):
+    train_X, train_y, test_X, test_y = count_newsgroups
+    clf = NBSVM()
+    clf.fit(train_X, train_y)
+    p = clf.predict(test_X)
+    assert accuracy_score(p, test_y) > 0.9
 
 
-def test_NBSVM_scores_well_on_test(tfidf_newsgroups):
-	train_X, train_y, test_X, test_y = tfidf_newsgroups
-	clf = NBSVM()
-	clf.fit(train_X, train_y)
-	p = clf.predict(test_X)
-	assert accuracy_score(p, test_y) > 0.9
+def test_NBSVM_scores_well_with_tfidf_features(tfidf_newsgroups):
+    train_X, train_y, test_X, test_y = tfidf_newsgroups
+    clf = NBSVM()
+    clf.fit(train_X, train_y)
+    p = clf.predict(test_X)
+    assert accuracy_score(p, test_y) > 0.9
